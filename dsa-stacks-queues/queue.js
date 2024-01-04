8ad15cd4401c1309f4dba9639c953ed0fd215fc9
papontemjetix <papontem@gmail.com>
2024-01-03 20:48:15 -0500

    queue tests passing

--- dsa-stacks-queues/queue.js
+++ dsa-stacks-queues/queue.js
@@ -1,46 +1,81 @@
 /** Node: node for a queue. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
-/** Queue: chained-together nodes where you can
- *  remove from the front or add to the back. */
-
+/** Queues - FIRST IN FIRST OUT
+ * Make a Queue class.
+ * It should include methods for enqueuing, dequeuing, peeking, and checking if the queue is empty.
+ * Make it throw an error if you try to dequeue from an empty queue.
+ *
+ * Queue: chained-together nodes where you can remove from the front or add to the back.
+ *
+ *
+ */
 class Queue {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+	/** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {
+	enqueue(val) {
+		const newNode = new Node(val);
 
-  }
+		if (this.isEmpty()) {
+			this.first = newNode;
 
-  /** dequeue(): remove the node from the start of the queue
-   * and return its value. Should throw an error if the queue is empty. */
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
 
-  dequeue() {
+			this.last = newNode;
+		}
 
-  }
+		this.size++;
+	}
 
-  /** peek(): return the value of the first node in the queue. */
+	/** dequeue(): remove the node from the start of the queue
+	 * and return its value. Should throw an error if the queue is empty. */
 
-  peek() {
+	dequeue() {
+		let result;
 
-  }
+		if (this.isEmpty()) {
+			throw new Error("QUEUE IS EMPTY!!!!");
+		} else {
+			result = this.first;
+			this.first = this.first.next;
+		}
 
-  /** isEmpty(): return true if the queue is empty, otherwise false */
+		this.size--;
 
-  isEmpty() {
+		return result.val;
+	}
 
-  }
+	/** peek(): return the value of the first node in the queue. */
+
+	peek() {
+		if (this.isEmpty()) {
+			return null;
+		}
+		return this.first.val;
+	}
+
+	/** isEmpty(): return true if the queue is empty, otherwise false */
+
+	isEmpty() {
+		if (this.size == 0 && !this.first) {
+			return true;
+		}
+		return false;
+	}
 }
 
 module.exports = Queue;
--- dsa-stacks-queues/stack.js
+++ dsa-stacks-queues/stack.js
@@ -1,46 +1,47 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
 }
 
-/** Stack: chained-together nodes where you can
- *  remove from the top or add to the top. */
+/** Stacks - LAST IN LAST OUT
+ * Make a Stack class.
+ * It should include methods for pushing, popping, peeking, and checking if the stack is empty.
+ * Make it throw an error if you try to pop from an empty stack.
+ *
+ * Stack: chained-together nodes where you can remove from the top or add to the top.
+ *
+ *
+ * */
 
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+	/** push(val): add new value to end of the stack. Returns undefined. */
 
-  push(val) {
+	push(val) {}
 
-  }
+	/** pop(): remove the node from the top of the stack
+	 * and return its value. Should throw an error if the stack is empty. */
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+	pop() {}
 
-  pop() {
+	/** peek(): return the value of the first node in the stack. */
 
-  }
+	peek() {
+		return this.first;
+	}
 
-  /** peek(): return the value of the first node in the stack. */
+	/** isEmpty(): return true if the stack is empty, otherwise false */
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+	isEmpty() {}
 }
 
 module.exports = Stack;
